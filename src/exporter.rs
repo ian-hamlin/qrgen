96eee768d3f839044b0f5dfa179b1c7d6a1a9750
ian-hamlin <ian@ihamlin.co.uk>
2019-05-02 15:01:16 +0100

    write png.

--- src/exporter.rs
+++ src/exporter.rs
@@ -1,5 +1,6 @@
 use crate::generator;
 use log::trace;
+use png::HasParameters;
 use std::{error::Error, fs::OpenOptions, io::prelude::*, path::PathBuf};
 
 pub struct Exporter {
@@ -68,40 +69,69 @@ impl Exporter {
 
     fn export_png<W: Write>(
         &self,
-        mut _writer: W,
+        writer: W,
         qr_code: &qrcodegen::QrCode,
         border: u8,
     ) -> Result<(), Box<Error>> {
         // ToDo - set a scale from the opts.
-        let scale: i32 = 10;
+        let scale: i32 = 20;
 
         // Get the size of the code. This is the base size before adding the borders and scale.
-        let size = qr_code.size().checked(scale, border);
+        let size = (qr_code.size() as u32).checked_size(scale, border);
 
-        trace!("check size {:?}", size);
+        // Multiple by three as RGB has 3 values.
+        let data_length = size.checked_length(3);
 
-        // Add the border.
-        //let size = size.checked_add(i32::from(border) * 2);
+        if size.is_some() && data_length.is_some() {
+            // Both are some, so this is OK.
+            let size = size.unwrap();
+            let data_length = data_length.unwrap();
 
-        // if size.is_none() {
-        //     return Err(Box::new(String::from(
-        //         "QR mask must be between 0 and 7 inclusive.",
-        //     )));
-        // }
+            let mut encoder = png::Encoder::new(writer, size, size);
+            encoder.set(png::ColorType::RGB).set(png::BitDepth::Eight);
+
+            let mut writer = encoder.write_header()?;
+            let mut data = vec![255_u8; data_length as usize];
+            let mut offset = 0_usize;
+            let border = i32::from(border);
+
+            for x in 0..size as i32 {
+                for y in 0..size as i32 {
+                    if qr_code.get_module(x / scale - border, y / scale - border) {
+                        data[offset] = 0;
+                        data[offset + 1] = 0;
+                        data[offset + 2] = 0;
+                    }
+                    offset += 3;
+                }
+            }
+
+            writer.write_image_data(&data)?
+        }
 
         Ok(())
     }
 }
 
 trait CheckedSize {
-    fn checked(self, scale: i32, border: u8) -> Option<i32>;
+    fn checked_size(self, scale: i32, border: u8) -> Option<u32>;
 }
 
-impl CheckedSize for i32 {
-    fn checked(self, scale: i32, border: u8) -> Option<i32> {
+impl CheckedSize for u32 {
+    fn checked_size(self, scale: i32, border: u8) -> Option<u32> {
         Some(
-            self.checked_add(i32::from(border) * 2)?
-                .checked_mul(scale)?,
+            self.checked_add(u32::from(border) * 2)?
+                .checked_mul(scale as u32)?,
         )
+    }
+}
+
+trait CheckdLength {
+    fn checked_length(self, colour_depth: u32) -> Option<u32>;
+}
+
+impl CheckdLength for Option<u32> {
+    fn checked_length(self, colour_depth: u32) -> Option<u32> {
+        Some(self?.checked_mul(self?)?.checked_mul(colour_depth)?)
     }
 }
